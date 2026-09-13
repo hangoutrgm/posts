929ee7e13ff786b5ce6a2f25cb93094c8cba3469
hangoutrgm <rpforcegaming@gmail.com>
2026-09-13 15:30:00 +0800

    Add files via upload
    more items for quiz type games

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hangout-v133';
+const CACHE_NAME = 'hangout-v134';
 
 // All local assets to pre-cache on install (relative paths for GitHub Pages subfolder & custom domain support)
 const PRECACHE_ASSETS = [
